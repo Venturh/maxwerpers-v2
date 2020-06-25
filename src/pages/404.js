9cfe44a66fa36e8dca9d3cc6f6dfd5c825fc0754
Venturh <venturh94@gmail.com>
2020-06-25 17:33:16 +0200

    fixed 404 bug

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 
 import Layout from "../components/templates/Layout"
 import Seo from "../components/atoms/Seo"
 
-const NotFoundPage = ({ data }) => (
-  <Layout>
-    <Seo title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = ({ data }) => {
+  const [section, setSection] = useState("error")
+  const sectionValue = { section, setSection }
+  return (
+    <Layout Layout section={sectionValue}>
+      <Seo title="404: Not found" />
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
