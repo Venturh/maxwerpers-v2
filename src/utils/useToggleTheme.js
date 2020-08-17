7fb881a0e70cadbf7e6d63984314f5ec267deb78
Venturh <venturh94@gmail.com>
2020-08-18 01:57:46 +0200

    removed saving of theme to storage, flickering issues

--- src/utils/useToggleTheme.js
+++ src/utils/useToggleTheme.js
@@ -1,19 +1,10 @@
 import { useState } from "react"
 
 export const useToggleTheme = () => {
-  const [theme, setTheme] = useState(
-    (typeof window !== "undefined" && window.localStorage.getItem("theme")) ||
-      "dark"
-  )
+  const [theme, setTheme] = useState("dark")
 
   const toggleTheme = () => {
-    if (theme === "light") {
-      window.localStorage.setItem("theme", "dark")
-      setTheme("dark")
-    } else {
-      window.localStorage.setItem("theme", "light")
-      setTheme("light")
-    }
+    theme === "light" ? setTheme("dark") : setTheme("light")
   }
 
   return [theme, toggleTheme]
